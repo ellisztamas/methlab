e7f07de8507388b0fbfb94e300ba240fb17614cd
Thomas Ellis <thomas.ellis@gmi.oeaw.ac.at>
2023-07-20 16:23:58 +0200

    Added __init__/.py

---
+++ epiclinestools/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for epiclinestools."""
+
+__author__ = """Tom Ellis"""
+__email__ = 'thomas.ellis@gmi.oeaw.ac.at'
+__version__ = '0.1.2'
+
+import argparse
+
+from epiclinestools.align_fastq_with_plate_positions import align_fastq_with_plate_positions
+
