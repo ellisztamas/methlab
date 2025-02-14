418d1fe4a373502370ae3416449bed4efbdaaa01
Thomas Ellis <thomas.ellis@gmi.oeaw.ac.at>
2025-02-14 09:32:12 +0100

    Removed ibdpainting because this has been moved to a package of its own.
     Changes to be committed:
    	modified:   methlab/__init__.py
    	deleted:    methlab/ibdpainting.py
    	deleted:    tests/test_ibdpainting.py

--- methlab/__init__.py
+++ methlab/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Tom Ellis"""
 __email__ = 'thomas.ellis@gmi.oeaw.ac.at'
-__version__ = '0.9.2'
+__version__ = '0.10.0'
 
 # import argparse
 
@@ -11,4 +11,3 @@ from methlab.CytosineCoverageFile import CytosineCoverageFile
 from methlab.BismarkSam import *
 from methlab.methylation_state import methylation_state
 from methlab.estimate_beta_parameters import estimate_beta_parameters
-from methlab.ibdpainting import *
